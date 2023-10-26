0dfe005b40ed119deb1b8465dff636cb6d23dd8c
Ahmad <ahmadking531049@gmail.com>
2023-10-26 21:12:21 +0500

    First Commit

---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,20 @@
+﻿namespace FormTagHeilpers.Models
+{
+    public class EmployeeModel
+    {
+        public string Name { get; set; }
+        public int EmployeeId { get; set; }
+
+        public string Designation { get; set; }
+
+        public int Age { get; set; } 
+
+        public Gender Gender { get; set; } 
+
+        public string Married { get; set; }
+    } 
+    public enum Gender
+    {
+        Male,Female
+    }
+}
